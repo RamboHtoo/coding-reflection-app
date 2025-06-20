32890ec01904b4e9f4a27d888acf3bcc46fb1700
Htoo Lin Htet <hlinhtet@fandm.edu>
2025-06-20 11:57:59 +0700

    Added feature to save git commit info as a note

--- main.py
+++ main.py
@@ -1,48 +1,51 @@
 import os
+import subprocess
 
-def show_menu():
-    print("\nWhat do you want to do?")
+def get_git_commit():
+    result = subprocess.run(
+        ["git", "show", "--oneline"],
+        stdout=subprocess.PIPE,
+        text=True
+    )
+    return result.stdout #Don’t just show the result in the terminal — capture it so I can use it in code.
+
+while True:
+    print("What do you want to do?")
     print("1. Add a note")
     print("2. View notes")
     print("3. Exit")
-
-def add_note():
-    note = input("Write your note: ")
-    filename = input("Enter a name for this note (no spaces): ")
-    with open(f"notes/{filename}.txt", "w") as file:
-        file.write(note)
-    print(f"Note saved as {filename}.txt")
-
-def view_notes():
-    files = os.listdir("notes")
-    if not files:
-        print("No notes found.")
-        return
-
-    print("\nYour notes:")
-    for file in files:
-        print(f"- {file}")
-    chosen = input("Enter the name of the note to read (without .txt): ")
-    filepath = f"notes/{chosen}.txt"
-    if os.path.exists(filepath):
-        with open(filepath, "r") as file:
-            content = file.read()
-            print(f"\n--- {chosen}.txt ---")
-            print(content)
-    else:
-        print("Note not found.")
-
-# Main program loop
-while True:
-    show_menu()
-    choice = input("Choose an option (1/2/3): ")
-
+    print("4. Get commit content")
+    choice = input("Choose an option: ")
     if choice == "1":
-        add_note()
+        print("You chose to add a note")
+        n = input("Write your note: ")
+        filename = input("Give this note a name (no spaces): ")
+        with open(f"notes/{filename}.txt", "w") as file:
+            file.write(n)
     elif choice == "2":
-        view_notes()
+        print("You chose to view notes")
+        files = os.listdir("notes")
+        print("Your notes:")
+        for file in files:
+            print("-", file)
+        chosen = input("Enter the name of the note to read (without .txt): ")
+        filepath = f"notes/{chosen}.txt"
+
+        if os.path.exists(filepath):
+            with open(filepath, "r") as file:
+                print(file.read())
+        else:
+            print("Note not found")
     elif choice == "3":
-        print("Goodbye!")
+        print("You chose to exit")
         break
+    elif choice == "4":
+        print(get_git_commit())
+        commit_content = get_git_commit()
+        filename = input("Give this commit a name: ")
+        with open(f"notes/{filename}.txt", "w") as file:
+            file.write(commit_content)
+        print(f"Commit note saved as {filename}.txt")
     else:
-        print("Invalid choice. Try again.")
+        print("Invalid option")
+
