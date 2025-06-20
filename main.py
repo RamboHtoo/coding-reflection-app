41c5b5347545b044a8b1ab53a892fef9820b435f
Htoo Lin Htet <hlinhtet@fandm.edu>
2025-06-20 12:15:40 +0700

    Organized code with funtions, added delete_note function

--- main.py
+++ main.py
@@ -1,6 +1,38 @@
 import os
 import subprocess
 
+def add_note():
+    n = input("Write your note: ")
+    filename = input("Give this note a name (no spaces): ")
+    with open(f"notes/{filename}.txt", "w") as file:
+        file.write(n)
+
+def view_notes():
+    files = os.listdir("notes")
+    print("Your notes:")
+    for file in files:
+        print("-", file)
+    chosen = input("Enter the name of the note to read (without .txt): ")
+    filepath = f"notes/{chosen}.txt"
+    if os.path.exists(filepath):
+        with open(filepath, "r") as file:
+            print(file.read())
+    else:
+        print("Note not found")
+
+def delete_note():
+    files = os.listdir("notes")
+    print("Your notes:")
+    for file in files:
+        print("-", file)
+    chosen = input("Enter the name of the note to delete (without .txt): ")
+    filepath = f"notes/{chosen}.txt"
+    if os.path.exists(filepath):
+        os.remove(filepath)
+        print("Deleted")
+    else:
+        print("Note not found")
+
 def get_git_commit():
     result = subprocess.run(
         ["git", "show", "--oneline"],
@@ -15,27 +47,14 @@ while True:
     print("2. View notes")
     print("3. Exit")
     print("4. Get commit content")
+    print("5. Delete a note")
     choice = input("Choose an option: ")
     if choice == "1":
         print("You chose to add a note")
-        n = input("Write your note: ")
-        filename = input("Give this note a name (no spaces): ")
-        with open(f"notes/{filename}.txt", "w") as file:
-            file.write(n)
+        add_note()
     elif choice == "2":
         print("You chose to view notes")
-        files = os.listdir("notes")
-        print("Your notes:")
-        for file in files:
-            print("-", file)
-        chosen = input("Enter the name of the note to read (without .txt): ")
-        filepath = f"notes/{chosen}.txt"
-
-        if os.path.exists(filepath):
-            with open(filepath, "r") as file:
-                print(file.read())
-        else:
-            print("Note not found")
+        view_notes()
     elif choice == "3":
         print("You chose to exit")
         break
@@ -46,6 +65,8 @@ while True:
         with open(f"notes/{filename}.txt", "w") as file:
             file.write(commit_content)
         print(f"Commit note saved as {filename}.txt")
+    elif choice == "5":
+        delete_note()
     else:
         print("Invalid option")
 
